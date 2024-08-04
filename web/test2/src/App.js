2931599e94dbd16b13da04dc812f46d4490dfc32
Sigge Clauss <siggeclauss@Sigges-Air.lan>
2024-08-04 14:04:38 +0200

    sizes

--- web/test2/src/App.js
+++ web/test2/src/App.js
@@ -1,4 +1,5 @@
 import './App.scss';
+import Footer from './molecules/footer';
 import Navbar from './molecules/navbar';
 
 function App() {
@@ -9,31 +10,36 @@ function App() {
       <section className="hero">
       {/* <div className='bigblur'></div> */}
 
-        <div className='box-container'>
-          {/* <h1>Trading made easy</h1> */}
-          <div className='herotitle'>
-            <span className='title-news'>
-              Promo 'ilovelean' for 15% off first purchase!
-            </span>
-            <h1>Quantitative Analysis for<br/> everyday people</h1>
-            <div className='cta-button'>
-              <a href='/plans'>
-                <span>
-                  Get started with Stocklighter
-                </span>
-              </a>
+        <div className='section-container'>
+          <div className='box-container'>
+            {/* <h1>Trading made easy</h1> */}
+            <div className='herotitle'>
+              <span className='title-news'>
+                Promo 'ilovelean' for 15% off first purchase!
+              </span>
+              <h1>Quantitative analysis for<br/> everyday people</h1>
+              <div className='cta-button'>
+                <a href='/plans'>
+                  <span>
+                    Get started with Stocklighter
+                  </span>
+                </a>
+              </div>
+            </div>
+            <div className='box'>
+              <h1>image of stocklighter in use</h1>
             </div>
           </div>
-          <div className='box'>
-            <h1>Place graph here</h1>
-          </div>
-        </div>
-      </section>
-      
-      <section className=''>
+      </div>
 
       </section>
-      
+        
+      <section className='testimonials default'>
+        <div className='section-container'>
+          <h2>Read our testimonials</h2>
+        </div>
+      </section>
+      <Footer></Footer>
     </div>
   );
 }
